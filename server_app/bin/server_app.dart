975de5db4eea648d4c70f5f1b64c9e339ca70bbe
Josue <josuecarlosos2@gmail.com>
2022-04-08 17:35:22 -0300

    finalização do server - socket - com teste

--- server_app/bin/server_app.dart
+++ server_app/bin/server_app.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:common/common.dart';
 import 'package:socket_io/socket_io.dart';
 
 void main(List<String> arguments) {
@@ -13,16 +14,42 @@ void main(List<String> arguments) {
 }
 
 void onConnection(Socket s) {
-  //vamos conectar todos a uma sala
-  //o socket vai escutar um evento
-  s.on('entrar_quarto', (data) {
-    //quando alguem se conectar, entendemos que ele esta enviando o nome e a sala que q entrar
-    final name = data['name'];
-    final room = data['room'];
+  //vamos conectar todos a uma sala //o socket vai escutar um evento
+  //quando alguem se conectar, entendemos que ele esta enviando o nome e a sala que q entrar
 
-    s.join(
-        room); //vamos juntar ele na sala para que apenas um grupo de socket se conect
-    s.to(room).broadcast.emit('message',
-        '${name} chegou'); //envia uma mensagem para todos da sala que alguém entrou. broadcast enviar para todos menos ele
-  });
+  s.on(
+    'entrar_quarto',
+    (data) {
+      final name = data['name'];
+      final room = data['room'];
+
+      s.join(room);
+      //vamos juntar ele na sala para que apenas um grupo de socket se conect
+
+      s.to(room).broadcast.emit(
+            'message',
+            SocketEventAReceber(
+              name: name,
+              room: room,
+              type: SocketEventEnum.enter_room,
+            ).toJson(),
+          ); //envia uma mensagem para todos da sala que alguém entrou. broadcast enviar para todos menos ele
+
+      s.on(
+        "disconnect",
+        (data) {
+          s.to(room).broadcast.emit(
+                "message",
+                SocketEventAReceber(
+                  name: name,
+                  room: room,
+                  type: SocketEventEnum.leave_room,
+                ).toJson(),
+              );
+        },
+      );
+
+      s.on("message", (data) => s.to(room).broadcast.emit("message", data));
+    },
+  );
 }
